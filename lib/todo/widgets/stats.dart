4c61d397593535c3e0904f449a91b493b12cc1ec
TunNandaAung <tunnandaaung@outlook.com>
2019-11-17 20:26:40 +0630

    Update Stat Screen Cards

--- lib/todo/widgets/stats.dart
+++ lib/todo/widgets/stats.dart
@@ -3,6 +3,7 @@ import 'package:firebase_integrations/todo/widgets/loading_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class Stats extends StatelessWidget {
   @override
@@ -16,77 +17,137 @@ class Stats extends StatelessWidget {
             child: Column(
               children: [
                 Padding(
-                  padding: const EdgeInsets.only(top: 30.0),
+                  padding: const EdgeInsets.all(12.0),
                   child: Container(
-                      height: 200,
-                      width: 200,
+                      height: 108,
+                      width: double.infinity,
                       decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20.0),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.black26,
-                              blurRadius: 10.0,
-                              offset: Offset(0, 10),
-                            )
-                          ],
-                          gradient: LinearGradient(
-                              colors: [Color(0xFF4B79A1), Color(0xFF283E51)])),
+                        borderRadius: BorderRadius.circular(20.0),
+                        color: Color(0xFF4293f5),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black54,
+                            blurRadius: 10.0,
+                            offset: Offset(0, 10),
+                          )
+                        ],
+                      ),
                       child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: <Widget>[
                           Padding(
-                            padding: EdgeInsets.only(top: 12.0, bottom: 30.0),
-                            child: Text(
-                              'Completed Todos',
-                              style: TextStyle(
-                                  fontFamily: 'Poppins-Medium',
-                                  color: Colors.white,
-                                  fontSize: 18.0),
+                            padding: EdgeInsets.only(
+                                left: 15.0, top: 12.0, bottom: 5.0),
+                            child: Row(
+                              children: <Widget>[
+                                Icon(
+                                  MdiIcons.checkAll,
+                                  color: Colors.grey[200],
+                                ),
+                                SizedBox(
+                                  width: 8.0,
+                                ),
+                                Text(
+                                  'Completed Todos',
+                                  style: TextStyle(
+                                      fontFamily: 'Poppins-Medium',
+                                      color: Colors.grey[200],
+                                      fontSize: 18.0),
+                                ),
+                              ],
                             ),
                           ),
                           Padding(
-                            padding: EdgeInsets.only(bottom: 0.0),
-                            child: Text(
-                              '${state.numCompleted}',
-                              style: TextStyle(
-                                  fontFamily: 'Poppins-Medium',
-                                  color: Colors.white,
-                                  fontSize: 30.0),
+                            padding: EdgeInsets.only(left: 21.0),
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.baseline,
+                              textBaseline: TextBaseline.alphabetic,
+                              children: <Widget>[
+                                Text(
+                                  '${state.numCompleted}',
+                                  style: TextStyle(
+                                      fontFamily: 'Poppins-Bold',
+                                      color: Colors.white,
+                                      fontSize: 36.0),
+                                ),
+                                SizedBox(width: 8.0),
+                                Text(
+                                  'Done',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 15.0),
+                                ),
+                              ],
                             ),
                           ),
                         ],
                       )),
                 ),
-                SizedBox(height: 50.0),
-                Container(
-                    height: 200,
-                    width: 200,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(30.0),
-                        color: Colors.white),
-                    child: Column(
-                      children: <Widget>[
-                        Padding(
-                          padding: EdgeInsets.only(bottom: 8.0),
-                          child: Text(
-                            'Active Todos',
-                            style: TextStyle(
-                                fontFamily: 'Poppins-Bold',
-                                color: Colors.white,
-                                fontSize: 25.0),
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(bottom: 24.0),
-                          child: Text(
-                            "${state.numActive}",
-                            style: TextStyle(
-                                fontFamily: 'Poppins-Bold',
-                                color: Colors.white,
-                                fontSize: 20.0),
-                          ),
-                        ),
-                      ],
-                    )),
+                Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Container(
+                        height: 108,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20.0),
+                            color: Color(0xFFd43759),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black54,
+                                blurRadius: 10.0,
+                                offset: Offset(0, 10),
+                              )
+                            ]),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Padding(
+                              padding: EdgeInsets.only(
+                                  left: 15.0, top: 12.0, bottom: 5.0),
+                              child: Row(
+                                children: <Widget>[
+                                  Icon(
+                                    MdiIcons.bellRing,
+                                    color: Colors.grey[200],
+                                  ),
+                                  SizedBox(
+                                    width: 8.0,
+                                  ),
+                                  Text(
+                                    'Active Todos',
+                                    style: TextStyle(
+                                        fontFamily: 'Poppins-Medium',
+                                        color: Colors.grey[200],
+                                        fontSize: 18.0),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(left: 21.0),
+                              child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.baseline,
+                                textBaseline: TextBaseline.alphabetic,
+                                children: <Widget>[
+                                  Text(
+                                    '${state.numActive}',
+                                    style: TextStyle(
+                                        fontFamily: 'Poppins-Bold',
+                                        color: Colors.white,
+                                        fontSize: 36.0),
+                                  ),
+                                  SizedBox(width: 8.0),
+                                  Text(
+                                    'Left',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                        color: Colors.white, fontSize: 15.0),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ))),
               ],
             ),
           );
